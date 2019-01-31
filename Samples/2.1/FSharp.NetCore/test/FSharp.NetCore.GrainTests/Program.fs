22d142654f20f43e648f538cc9a7abbf583490a8
jbrestan <jan.brestan@live.com>
2019-01-31 17:04:00 +0100

    Add Orleankka to reproduce the failing generator

--- Samples/2.1/FSharp.NetCore/test/FSharp.NetCore.GrainTests/Program.fs
+++ Samples/2.1/FSharp.NetCore/test/FSharp.NetCore.GrainTests/Program.fs
@@ -5,21 +5,12 @@ open Orleans
 open Orleans.Hosting
 open Orleans.TestingHost
 open Xunit
-
 open FSharp.NetCore.Grains
 open FSharp.NetCore.Interfaces
 open Microsoft.Extensions.Configuration
+open Orleankka.Client
+open Orleankka.Cluster
 
-let buildSiloHost () =
-      let builder = new SiloHostBuilder()
-      builder
-        .UseLocalhostClustering()
-        .ConfigureApplicationParts(fun parts ->
-            parts.AddApplicationPart(typeof<HelloGrain>.Assembly)
-                  .AddApplicationPart(typeof<IHello>.Assembly)
-                  .WithCodeGeneration() |> ignore)
-        .ConfigureLogging(fun logging -> logging.AddConsole() |> ignore)
-        .Build()
 
 type ClientConfigurator () =
     interface IClientBuilderConfigurator with
@@ -30,18 +21,21 @@ type ClientConfigurator () =
                         .AddApplicationPart(typeof<IHello>.Assembly)
                         .WithCodeGeneration()
                     |> ignore)
+                .UseOrleankka()
                 |> ignore
 
 type SiloConfigurator () =
     interface ISiloBuilderConfigurator with
         member this.Configure (siloBuilder: ISiloHostBuilder) =
             siloBuilder
+                .UseInMemoryReminderService()
                 .ConfigureApplicationParts(fun appParts ->
                     appParts
                         .AddApplicationPart(typeof<HelloGrain>.Assembly)
                         .AddApplicationPart(typeof<IHello>.Assembly)
                         .WithCodeGeneration()
                     |> ignore)
+                .UseOrleankka()
                 |> ignore
 
 type Tests () =
